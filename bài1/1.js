e12a8af28a64669ed9699447071210ce4971db90
KY <nguyenhoangky23121997@gmail.com>
2023-03-11 17:25:14 +0700

    01

--- bài1/1.js
+++ bài1/1.js
@@ -3,6 +3,8 @@ let m=prompt("Diện tích")
     p=n/m
 if(p>1000){
     alert("Dân số quá đông đúc")
-}else {
+}else if(p>=0&&p<=1000){
     alert("Dân số không quá đông đúc")
+}else {
+    alert("dữ liệu không thực tế")
 }
