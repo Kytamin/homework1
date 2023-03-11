5cc60aec3973d3f31fbd5a4962f1f7c3bd172498
KY <nguyenhoangky23121997@gmail.com>
2023-03-12 01:32:06 +0700

    02

--- 5/5.js
+++ 5/5.js
@@ -1,16 +1,21 @@
 function handle(){
-    let a=document.getElementById("entera").value
-    let b=document.getElementById("enterb").value
-    let c=document.getElementById("enterc").value
+    let a=+document.getElementById("entera").value
+    let b=+document.getElementById("enterb").value
+    let c=+document.getElementById("enterc").value
     let delta=(b*b)-(4*a*c)
-    if(delta>0){
+    if(a===0&&b===0&&c===0){
+        document.getElementById("result").innerHTML="Xác định giá trị cho a,b,c"
+    }else if(a===0){
+        v=(-c/b)
+        document.getElementById("result").innerHTML=`Phương trình có nghiệm x=${v}`
+    }else if(delta>0){
         x=((-b+Math.sqrt(delta))/2*a)
         z=((-b-Math.sqrt(delta))/2*a)
         document.getElementById("result").innerHTML=`Phương trình có 2 nghiệm x1=${x} và x2=${z} `
     }else if(delta===0){
         n=-(b/(2*a))
         document.getElementById("result").innerHTML=`Phương trình có nghiệm kép x1=x2=${n}`
-    }else {
-        document.getElementById("result").innerHTML=`Phương trình vô nghiệm`
+    }else  {
+        document.getElementById("result").innerHTML = `Phương trình vô nghiệm`
     }
 }
